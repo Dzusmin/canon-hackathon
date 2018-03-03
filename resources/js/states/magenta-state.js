b431fadd225bbd81d2009307eda3f2fc9b8fab34
Peter Cyra <contact@pcyra.pl>
2018-03-03 19:06:15 +0100

    magenta fix

--- resources/js/states/magenta-state.js
+++ resources/js/states/magenta-state.js
@@ -6,13 +6,12 @@ var MAGENTA_STATE = (function()
 		create: function()
 		{
 			GAME.stage.backgroundColor = '#FF00FF';
-			GAME.input.onDown.add();
+			GAME.input.onDown.add(function()
+			{
+				GAME.state.start('TMP_STATE');
+			});
 		}
 	};
 	
 	GAME.state.add('MAGENTA_STATE', STATE);
-	
-	return {
-		setPointerPosition: setPointerPosition
-	}
 })();
